cf6e01eac72fa55056d357a0cd72e2d73f4b7da2
rgurgul <gurgul.robert@gmail.com>
2021-06-30 08:04:03 +0200

    tests

--- src/app/shared/directives/set-color.directive.spec.ts
+++ src/app/shared/directives/set-color.directive.spec.ts
@@ -1,8 +1,40 @@
-import { SetColorDirective } from './set-color.directive';
+import { TestBed } from "@angular/core/testing";
+import { SetColorDirective } from "./set-color.directive";
+import { Component } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
-describe('SetColorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SetColorDirective();
-    expect(directive).toBeTruthy();
+@Component({
+  selector: 'wrapper-component'
+})
+
+class WrapperComponent { }
+
+describe('set color directive', () => {
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        WrapperComponent,
+        SetColorDirective
+      ]
+    });
   });
+
+  it('should set wrapper bg color', () => {
+    const bgColor = 'blue';
+
+    TestBed.overrideComponent(WrapperComponent, {
+      set: {
+        template: `<div [appSetColor]="'${bgColor}'"></div>`
+      }
+    });
+
+    const fixtureWrapperComponent = TestBed.createComponent(WrapperComponent);
+    const element = fixtureWrapperComponent.debugElement;
+    fixtureWrapperComponent.detectChanges();
+
+    const setColorDirectiveHost = element.query(By.directive(SetColorDirective));
+    expect(setColorDirectiveHost.styles['background-color']).toBe(bgColor);
+  });
+
 });
